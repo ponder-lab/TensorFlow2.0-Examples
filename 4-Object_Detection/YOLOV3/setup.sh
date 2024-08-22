4e3f8a1551228f8def26746f8732d17a2e54c8cb
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-08-22 11:49:37 -0400

    Check if the files exist first during setup.

--- 4-Object_Detection/YOLOV3/setup.sh
+++ 4-Object_Detection/YOLOV3/setup.sh
@@ -3,5 +3,10 @@ set -ex
 
 PYTHON="/usr/local/bin/python3.10"
 
-$PYTHON yymnist/make_data.py --images_num 1000 --images_path ./data/dataset/train --labels_txt ./data/dataset/yymnist_train.txt
-$PYTHON yymnist/make_data.py --images_num 200  --images_path ./data/dataset/test  --labels_txt ./data/dataset/yymnist_test.txt
+if [[ ! -f "./data/dataset/yymnist_train.txt" ]]; then
+    $PYTHON yymnist/make_data.py --images_num 1000 --images_path ./data/dataset/train --labels_txt ./data/dataset/yymnist_train.txt
+fi
+
+if [[ ! -f "./data/dataset/yymnist_test.txt" ]]; then
+    $PYTHON yymnist/make_data.py --images_num 200  --images_path ./data/dataset/test --labels_txt ./data/dataset/yymnist_test.txt
+fi
