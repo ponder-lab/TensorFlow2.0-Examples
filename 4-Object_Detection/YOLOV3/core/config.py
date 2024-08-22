eb4d98b5c9d044e4736a255921fcddbfc1c0f973
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-08-22 10:32:25 -0400

    Use absolute paths.

--- 4-Object_Detection/YOLOV3/core/config.py
+++ 4-Object_Detection/YOLOV3/core/config.py
@@ -12,6 +12,7 @@
 #================================================================
 
 from easydict import EasyDict as edict
+import os
 
 
 __C                           = edict()
@@ -23,8 +24,10 @@ cfg                           = __C
 __C.YOLO                      = edict()
 
 # Set the class name
-__C.YOLO.CLASSES              = "./data/classes/coco.names"
-__C.YOLO.ANCHORS              = "./data/anchors/basline_anchors.txt"
+project_directory             = os.path.dirname(os.path.dirname(__file__))
+
+__C.YOLO.CLASSES              = os.path.join(project_directory, "data/classes/coco.names")
+__C.YOLO.ANCHORS              = os.path.join(project_directory, "data/anchors/basline_anchors.txt")
 __C.YOLO.STRIDES              = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE     = 3
 __C.YOLO.IOU_LOSS_THRESH      = 0.5
@@ -32,7 +35,7 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 # Train options
 __C.TRAIN                     = edict()
 
-__C.TRAIN.ANNOT_PATH          = "./data/dataset/yymnist_train.txt"
+__C.TRAIN.ANNOT_PATH          = os.path.join(project_directory, "data/dataset/yymnist_train.txt")
 __C.TRAIN.BATCH_SIZE          = 4
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = [416]
@@ -47,11 +50,11 @@ __C.TRAIN.EPOCHS              = 30
 # TEST options
 __C.TEST                      = edict()
 
-__C.TEST.ANNOT_PATH           = "./data/dataset/yymnist_test.txt"
+__C.TEST.ANNOT_PATH           = os.path.join(project_directory, "data/dataset/yymnist_test.txt")
 __C.TEST.BATCH_SIZE           = 2
 __C.TEST.INPUT_SIZE           = 544
 __C.TEST.DATA_AUG             = False
-__C.TEST.DECTECTED_IMAGE_PATH = "./data/detection/"
+__C.TEST.DECTECTED_IMAGE_PATH = os.path.join(project_directory, "data/detection/")
 __C.TEST.SCORE_THRESHOLD      = 0.3
 __C.TEST.IOU_THRESHOLD        = 0.45
 
